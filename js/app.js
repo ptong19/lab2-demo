dec6ce16d8631993455678b976c05d2b1bb90781
ptong19 <kindaconan@gmail.com>
2019-05-06 20:56:51 -0700

    initial  file setup

---
+++ js/app.js
@@ -0,0 +1,29 @@
+var likesGamesOfThrones = prompt(' Do you like Game of Thornes?');
+
+
+likesGamesOfThrones = likesGamesOfThrones.toLocaleLowerCase();
+
+
+
+if (likesGamesOfThrones === 'yes' )
+{
+    alert('me too!');
+} else  {
+    alert( 'how can u not like')
+}
+
+
+
+var thaiFood = prompt('Do you like Thai foods?');
+
+thaiFood = thaiFood.toLocaleLowerCase();
+
+
+if(thaiFood === 'yes')
+{
+    alert('awesome!!!!!!!!!!!!! :)');
+
+} else {
+
+    alert( 'Why Why why')
+}
